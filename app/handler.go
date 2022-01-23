a8b483795ff36f8cc5c55360006b131caa3a12bd
yqlbu <yqlbu@bu.edu>
2022-01-24 07:47:56 +0800

    removed unnecessary lines

--- app/handler.go
+++ app/handler.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -12,32 +12,23 @@ func Handler(res http.ResponseWriter, req *http.Request) {
 	// First, decode the JSON response body
 	body := &WebhookReqBody{}
 	if err := json.NewDecoder(req.Body).Decode(body); err != nil {
-		fmt.Println("could not decode request body", err)
+		log.Println("could not decode request body", err)
 		return
 	}
 
-	// Check if the message contains the word "marco"
-	// if not, return without doing aything
 	if !strings.Contains(strings.ToLower(body.Message.Text), "hi") {
 		return
 	}
 
-	// If the text contains marco, call the `sayPolo` function, which
-	// is defined below
 	if err := SayHi(body.Message.Chat.ID); err != nil {
-		fmt.Println("error in sending reply:", err)
+		log.Println("error in sending reply:", err)
 		return
 	}
 
 	// log a confirmation message if the message is sent successfully
-	fmt.Println("reply sent")
+	log.Println("reply sent")
 }
 
-//The below code deals with the process of sending a response message to the user
-
-// sayHi takes a chatID and sends "nihao" to them
-
-// FInally, the main funtion starts our server on port 3000
 func main() {
 	http.ListenAndServe(":3000", http.HandlerFunc(Handler))
 }
